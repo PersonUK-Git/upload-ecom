c4b334d1d8adfc02fbafa2c722a3125df6c62295
prateek sharma <ppprateeksharma006@gmail.com>
2024-03-26 13:12:34 +0530

    all things added and new link is set

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -19,7 +19,7 @@ const Users = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const { data } = await axios.post('http://localhost:8080/api/v1/category/create-category', { name })
+      const { data } = await axios.post('https://node-server-s44q.onrender.com/api/v1/category/create-category', { name })
       if (data?.success) {
         toast.success(`${name} is created`)
         getAllCategory()
@@ -35,7 +35,7 @@ const Users = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get('http://localhost:8080/api/v1/category/get-category')
+      const { data } = await axios.get('https://node-server-s44q.onrender.com/api/v1/category/get-category')
       if (data?.success) {
         setCategories(data?.category)
       }
@@ -53,7 +53,7 @@ const Users = () => {
   const handleUpdate = async (e) => {
     e.preventDefault()
     try {
-      const { data } = await axios.put(`http://localhost:8080/api/v1/category/update-category/${selected._id}`, { name: updatedName })
+      const { data } = await axios.put(`https://node-server-s44q.onrender.com/api/v1/category/update-category/${selected._id}`, { name: updatedName })
       if (data.success) {
         toast.success(`${updatedName} is updated`)
         setSelected(null)
@@ -73,7 +73,7 @@ const Users = () => {
   const handleDelete = async (pId) => {
 
     try {
-      const { data } = await axios.delete(`http://localhost:8080/api/v1/category/delete-category/${pId}`)
+      const { data } = await axios.delete(`https://node-server-s44q.onrender.com/api/v1/category/delete-category/${pId}`)
       if (data.success) {
         toast.success(`category is deleted`)
 
--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -26,7 +26,7 @@ const ProductDetails = () => {
   const getProduct = async () => {
     try {
       const { data } = await axios.get(
-        `http://localhost:8080/api/v1/product/get-product/${params.slug}`
+        `https://node-server-s44q.onrender.com/api/v1/product/get-product/${params.slug}`
       );
       setProduct(data?.product);
       getSimilarProduct(data?.product._id, data?.product.category._id);
@@ -38,7 +38,7 @@ const ProductDetails = () => {
   const getSimilarProduct = async (pid, cid) => {
     try {
       const { data } = await axios.get(
-        `http://localhost:8080/api/v1/product/related-product/${pid}/${cid}`
+        `https://node-server-s44q.onrender.com/api/v1/product/related-product/${pid}/${cid}`
       );
       setRelatedProducts(data?.products);
     } catch (error) {
@@ -50,7 +50,7 @@ const ProductDetails = () => {
       <div className="row container product-details">
         <div className="col-md-6">
           <img
-            src={`http://localhost:8080/api/v1/product/product-photo/${product._id}`}
+            src={`https://node-server-s44q.onrender.com/api/v1/product/product-photo/${product._id}`}
             className="card-img-top"
             alt={product.name}
             height="300"
@@ -90,7 +90,7 @@ const ProductDetails = () => {
           {relatedProducts?.map((p) => (
             <div className="card m-2" key={p._id}>
               <img
-                src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                src={`https://node-server-s44q.onrender.com/api/v1/product/product-photo/${p._id}`}
                 className="card-img-top"
                 alt={p.name}
               />
